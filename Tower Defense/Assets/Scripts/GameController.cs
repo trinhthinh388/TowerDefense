37c2c1b155c89a530937d35173a2fc6862faf7af
Trinh Duc Thinh <trinhthinh388@gmail.com>
2018-05-13 10:35:14 +0700

    Change

---
+++ Tower Defense/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Transform EnemyPreFab;
+	public Transform SpawnPoint;
+	public float TimeBetweenSpawn = 6f;
+	public float countdown = 0;
+	public int WaveNumbers = 0;
+	public Text SpawningText;
+	// Use this for initialization
+	void Update() {
+		if(countdown <= 0)
+		{
+			StartCoroutine(WaveSpawner());
+			countdown = TimeBetweenSpawn;
+		}
+		countdown -= Time.deltaTime;     
+		SpawningText.text = Mathf.Floor(countdown).ToString();
+
+	}
+
+
+    IEnumerator WaveSpawner()
+	{
+		WaveNumbers++;
+		for (int i = 0; i < WaveNumbers; i++)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+    void SpawnEnemy()
+	{
+		Instantiate(EnemyPreFab, SpawnPoint.position, SpawnPoint.rotation);
+	}
+}
---
+++ Tower Defense/Assets/Scripts/WayPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour {
+
+
+    public static Transform[] points;
+	// Use this for initialization
+	void Awake () {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+			Debug.Log(i);
+			points[i] = transform.GetChild(i);
+        }
+		
+	}
+}
